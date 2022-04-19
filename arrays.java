52537b75a0d6a5518f27d34add8974b8b73206b8
Agam <agamjotsinghreen@gmail.com>
2022-04-19 22:57:23 +0530

    Update arrays.java

--- arrays.java
+++ arrays.java
@@ -136,4 +136,286 @@ public void merge(int arr1[], int arr2[], int n, int m) {
         
     }
 
+    ///-> count inversions
+
+
+     static long merge( int l , int m , int r , long[] arr ){
+        int i = 0 , j = 0 , k = l ;
+        long left[] = Arrays.copyOfRange( arr , l , m + 1 );
+        long swap = 0 ; 
+         long right[] = Arrays.copyOfRange( arr , m + 1 , r + 1 );
+        while( i < left.length && j < right.length  ){
+            if(left[i]  <=right[j]){
+                arr[k++] = left[i++];
+                
+            }
+            else{
+              arr[k++] = right[j++];  
+              swap = (m + 1 ) - ( l + i);    
+            }
+            
+            
+            
+            
+            
+        }
+        while( i < left.length ){
+            arr[k++] = left[i++];
+        }
+        while( j < right.length ){
+            arr[k++] = right[j++];
+        }
+        
+        return swap;
+        
+        
+    }
+    
+    
+    static long mergesortAndCount( int l , int r , long arr[]){
+        long count  = 0 ; 
+        if( l < r){
+            
+            int m  = (l + r)/2;
+            
+            count +=mergesortAndCount( l , m , arr);
+            count +=mergesortAndCount( m + 1 , r , arr);
+            
+            count += merge( l , m , r , arr);
+            
+            
+            
+            
+        }
+        
+        
+        return count ; 
+        
+        
+    }
+    
+    
+    static long inversionCount(long arr[], long N)
+    {
+      return  mergesortAndCount( 0 , arr.length - 1 , arr );
+    }
+
+
+    // count pairs with given sum (not two sum)
+
+      int getPairsCount(int[] arr, int n, int k) {
+    HashMap<Integer,Integer> m = new HashMap<>();
+    int count = 0;
+    for (int i = 0; i < n; i++) {
+        if (m.containsKey(k - arr[i])) {
+            count += m.get(k - arr[i]);
+        }
+        if(m.containsKey(arr[i])){
+            m.put(arr[i], m.get(arr[i])+1);
+        }
+        else{
+            m.put(arr[i], 1);
+        }
+    }
+    return count;
+    }
+    // common elements
+     ArrayList<Integer> commonElements(int ar1[], int ar2[], int ar3[], int n1, int n2, int n3) 
+    {
+      
+        // Initialize starting indexes for ar1[], ar2[] and
+        // ar3[]
+        int i = 0, j = 0, k = 0;
+          ArrayList<Integer> list = new ArrayList<>();
+        // Iterate through three arrays while all arrays
+        // have elements
+        while (i < ar1.length && j < ar2.length
+               && k < ar3.length) {
+            // If x = y and y = z, print any of them and
+            // move ahead in all arrays
+        
+            if (ar1[i] == ar2[j] && ar2[j] == ar3[k]) {
+                if(!list.contains(ar1[i])){
+                list.add(ar1[i]);
+                }
+                i++;
+                j++;
+                k++;
+            }
+
+            // x < y
+            else if (ar1[i] < ar2[j])
+                i++;
+
+            // y < z
+            else if (ar2[j] < ar3[k])
+                j++;
+
+            // We reach here when x > y and z < y, i.e., z
+            // is smallest
+            else
+                k++;
+        }
+        return list;
+
+}
+
+// sub array with sum 0
+
+static boolean findsum(int arr[],int n)
+    {
+      HashSet<Integer> set = new HashSet<>();
+      int sum= 0 ; 
+      for( int i = 0 ; i < arr.length ; i ++){
+         sum+=arr[i];
+         if(sum == 0 || arr[i] == 0 || set.contains(sum)){
+             return true;
+         }
+          
+          set.add(sum);
+      }
+      return false;
+    }
+// maximum product subarray 
+   long maxProduct(int[] arr, int n) {
+        
+        long mxv = arr[0];
+        long mnv = arr[0];
+        long mp = arr[0];
+        for( int i = 1 ; i <  n ; i ++){
+            
+            if( arr[i] < 0 ){
+                long temp = mxv ; 
+                mxv = mnv ; 
+                mnv = temp;
+            }
+            mxv = Math.max( arr[i] , arr[i] * mxv);
+            mnv = Math.min( arr[i] , arr[i] * mnv);
+            mp = Math.max( mxv , mp);
+            
+        }
+        
+       return mp; 
+        
+    }
+
+
+// longest subsequence present in an array O(n) approaches  space O(n)
+   // approach 1 using hashset
+	static int findLongestConseqSubseq(int arr[], int n)
+	{
+	   int ans = 0 ;
+	   HashSet<Integer> set = new HashSet<>();
+	   
+	   for( Integer i : arr){
+	      set.add(i);    
+	   }
+	   for( int i = 0 ; i <  n ; i ++){
+	     if(!set.contains(arr[i] - 1)){
+	       int j = arr[i];
+	       while( set.contains(j) ){
+	           j++;
+	       }
+	       if( ans < j - arr[i]){
+	           ans = j - arr[i];
+	       }
+	       
+	     }
+	   
+	       
+	       
+	       
+	       
+	   }
+	   return ans;
+	}
+
+   //approach 2 using priorityqueue
+
+
+    static int findLongestConseqSubseq(int arr[], int N)
+    {
+
+        PriorityQueue<Integer> pq
+            = new PriorityQueue<Integer>();
+        for (int i = 0; i < N; i++) 
+        {
+            // adding element from 
+            // array to PriorityQueue
+            pq.add(arr[i]);
+        }
+        
+        // Storing the first element 
+        // of the Priority Queue
+        // This first element is also 
+        // the smallest element
+        int prev = pq.poll();
+        
+        // Taking a counter variable with value 1
+        int c = 1;
+        
+        // Storing value of max as 1
+        // as there will always be
+        // one element
+        int max = 1;
+
+        for (int i = 1; i < N; i++) 
+        {
+            // check if current peek 
+            // element minus previous
+            // element is greater then 
+            // 1 This is done because
+            // if it's greater than 1 
+            // then the sequence
+            // doesn't start or is broken here
+            if (pq.peek() - prev > 1) 
+            {
+                // Store the value of counter to 1
+                // As new sequence may begin
+                c = 1;
+                
+                // Update the previous position with the
+                // current peek And remove it
+                prev = pq.poll();
+            }
+            
+            // Check if the previous
+            //  element and peek are same
+            else if (pq.peek() - prev == 0) 
+            {
+                // Update the previous position with the
+                // current peek And remove it
+                prev = pq.poll();
+            }
+            // if the difference 
+            // between previous element and peek is 1
+            else 
+            {
+                // Update the counter
+                // These are consecutive elements
+                c++;
+                 
+                // Update the previous position
+                //  with the current peek And remove it
+                prev = pq.poll();
+            }
+
+            // Check if current longest 
+            // subsequence is the greatest
+            if (max < c) 
+            {
+                // Store the current subsequence count as
+                // max
+                max = c;
+            }
+        }
+
+        return max;
+    }
+    
+
+
+
+
+
  
