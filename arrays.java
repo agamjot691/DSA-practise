c3bd0ca50982a750ef57f8da2388317cd0c54e68
Agam <agamjotsinghreen@gmail.com>
2022-04-20 16:19:13 +0530

    Update arrays.java

--- arrays.java
+++ arrays.java
@@ -1,3 +1,6 @@
+
+import java.util.*;
+public class arrays{
 // question -> merge sorted arrays without space  O(m + n )
 public int nextgap(int gap){
     if( gap<=1){
@@ -85,21 +88,22 @@ public void merge(int arr1[], int arr2[], int n, int m) {
         list.add(new int[]{start , end });
         
         return list.toArray(new int[list.size()][]);
+    }
         
     // -> max stock buy and sell at most two times
 
     int maxtwobuysell(int arr[],int size) {
-    int first_buy = INT_MIN;
+    int first_buy = Integer.MIN_VALUE;
       int first_sell = 0;
-      int second_buy = INT_MIN;
+      int second_buy = Integer.MIN_VALUE;
       int second_sell = 0;
        
       for(int i=0;i<size;i++) {
          
-          first_buy = max(first_buy,-arr[i]);//we set prices to negative, so the calculation of profit will be convenient
-          first_sell = max(first_sell,first_buy+arr[i]);
-          second_buy = max(second_buy,first_sell-arr[i]);//we can buy the second only after first is sold
-          second_sell = max(second_sell,second_buy+arr[i]);
+          first_buy = Math.max(first_buy,-arr[i]);//we set prices to negative, so the calculation of profit will be convenient
+          first_sell = Math.max(first_sell,first_buy+arr[i]);
+          second_buy = Math.max(second_buy,first_sell-arr[i]);//we can buy the second only after first is sold
+          second_sell = Math.max(second_sell,second_buy+arr[i]);
        
     }
      return second_sell;
@@ -302,7 +306,7 @@ static boolean findsum(int arr[],int n)
 
 // longest subsequence present in an array O(n) approaches  space O(n)
    // approach 1 using hashset
-	static int findLongestConseqSubseq(int arr[], int n)
+	static int findLongestConseqSubseq1(int arr[], int n)
 	{
 	   int ans = 0 ;
 	   HashSet<Integer> set = new HashSet<>();
@@ -412,10 +416,189 @@ static boolean findsum(int arr[],int n)
 
         return max;
     }
+//trappingWater
     
+    
+ static long trappingWater(int arr[], int n) { 
+             // left[i] contains height of tallest bar to the
+        // left of i'th bar including itself
+        int left[] = new int[n];
+
+        // Right [i] contains height of tallest bar to
+        // the right of ith bar including itself
+        int right[] = new int[n];
+
+        // Initialize result
+        long water = 0;
+
+        // Fill left array
+        left[0] = arr[0];
+        for (int i = 1; i < n; i++)
+            left[i] = Math.max(left[i - 1], arr[i]);
+
+        // Fill right array
+        right[n - 1] = arr[n - 1];
+        for (int i = n - 2; i >= 0; i--)
+            right[i] = Math.max(right[i + 1], arr[i]);
+
+        // Calculate the accumulated water element by element
+        // consider the amount of water on i'th bar, the
+        // amount of water accumulated on this particular
+        // bar will be equal to min(left[i], right[i]) - arr[i] .
+        for (int i = 0; i < n; i++)
+            water += Math.min(left[i], right[i]) - arr[i];
+
+        return water;
+    }
+  
+
+//  
+
+    public long findMinDiff (ArrayList<Long> a, long n, long m)
+    {
+       Collections.sort(a);
+       long sum  = (int)1e9;
+       for( int i = 0 ; i + m - 1< n ; i ++){
+          sum = Math.min( sum , ( a.get((int)(i + m - 1) ) - a.get((int)(i )))); 
+       }
+       return sum ; 
+    }
 
 
 
+//smallest subarray sum with given value
+
+public int minSubArrayLen(int target, int[] nums) {
+    int start = 0 , minlength = Integer.MAX_VALUE ; 
+      int sum = 0 ; 
+      for( int end = 0 ; end < nums.length ; end++){
+          sum +=nums[end];
+          while(sum >=target ){
+              
+              minlength = Math.min( minlength  , end - start + 1);
+              sum -= nums[start++];
+              
+          }
+          
+      }
+      
+      return minlength == Integer.MAX_VALUE ? 0 : minlength;
+      
+  }
+
+  public static int minSwap(int arr[], int n, int k) {
+ 
+    // Find count of elements which are
+    // less than equals to k
+    int count = 0;
+    for (int i = 0; i < n; i++)
+    if (arr[i] <= k)
+        ++count;
+ 
+    // Find unwanted elements in current
+    // window of size 'count'
+    int bad = 0;
+    for (int i = 0; i < count; i++)
+    if (arr[i] > k)
+        ++bad;
+
+    // Initialize answer with 'bad' value of
+    // current window
+    int ans = bad;
+    for (int i = 0, j = count; j < n; i++, j++) {
+ 
+    // Decrement count of previous window
+    if (arr[i] > k)
+        --bad;
+ 
+    // Increment count of current window
+    if (arr[j] > k)
+        ++bad;
+ 
+    // Update ans if count of 'bad'
+    // is less in current window
+    ans = Math.min(ans, bad);
+    }
+    return ans;
+    
+}
+
+
+// median of two sorted arrays of unequal lentgth 
+
+
+public double s(int[] ar1, int[] ar2) {
+        
+    // Current index of input array ar1[]
+    int i = 0;
+     int n = ar1.length ; 
+        int m = ar2.length ; 
+    // Current inde of input array ar2[]
+    int j = 0;
+    int count;
+    double m1 = -1, m2 = -1;
+     
+    // Since there are (n+m) elements, 
+    // There are following two cases 
+    // if n+m is odd then the middle 
+    //index is median i.e. (m+n)/2 
+    if ((m + n) % 2 == 1)
+    {
+        for(count = 0;
+            count <= (n + m) / 2;
+            count++)
+        {
+            if (i != n && j != m)
+            {
+                m1 = (ar1[i] > ar2[j]) ?
+                      ar2[j++] : ar1[i++];
+            }
+            else if (i < n)
+            {
+                m1 = ar1[i++];
+            }
+             
+            // for case when j<m,
+            else
+            {
+                m1 = ar2[j++];
+            }
+        }
+        return m1;
+    }
+ 
+    // median will be average of elements
+    // at index ((m+n)/2 - 1) and (m+n)/2
+    // in the array obtained after merging
+    // ar1 and ar2
+    else
+    {   
+        for(count = 0;
+            count <= (n + m) / 2;
+            count++)
+        {
+            m2 = m1;
+            if (i != n && j != m)
+            {
+                m1 = (ar1[i] > ar2[j]) ?
+                      ar2[j++] : ar1[i++];
+            }
+            else if (i < n)
+            {
+                m1 = ar1[i++];
+            }
+             
+            // for case when j<m,
+            else
+            {
+                m1 = ar2[j++];
+            }
+        }
+        return (m1 + m2) / 2 ;
+    }
+
+ }
+}
 
 
  
